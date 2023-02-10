19972f67e7bbbf9e9b21a44cda17065828d1f3b9
HARSHAL-AP <apsundeharshal129@gmail.com>
2023-02-10 22:42:17 +0530

    Medicin model created

---
+++ models/Medicin.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+  image: String,
+  title: String,
+  saleprice: Number,
+  category: String,
+  price: Number,
+  discount: Number,
+  subcategory: String,
+  rating: Number,
+});
+
+const ProductModel = mongoose.model("products", productSchema);
+
+module.exports = {
+  ProductModel,
+};
+ 
